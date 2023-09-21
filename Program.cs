0fc32bb0e8659968e71a56ff6bde96b34c1d9427
KittenWolf <FonarevAlexander@yandex.ru>
2023-09-21 22:08:17 +0300

    Added TripletAnalyzer class to find all matches, also added SortMatches and TakeMatches methods to this class.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using Solution;
+using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Stopwatch sw = new();
+        sw.Start();
+
+        TripletAnalyzer ta = new(@"???", 2);
+
+        ta.FindAllMatches();
+        ta.SortMatches();
+        var matches = ta.TakeMatches(10);
+
+        Console.WriteLine("RunTime: {0}ms", sw.ElapsedMilliseconds);
     }
 }
